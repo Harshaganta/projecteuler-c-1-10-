48afe37b1599492850cf61a0907fc545252a211c
Harshaganta <31781588+Harshaganta@users.noreply.github.com>
2017-12-28 16:16:10 -0800

    uploaded successfully

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int a = 0; int b = 1;int  c = 0 ;int d = 0;
+            while(b <= 4000000)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                
+                if(c <= 4000000 && c % 2 == 0)
+                {
+                    Console.WriteLine(c);
+                    d = d + c;
+                   
+                }
+                
+            }
+            Console.WriteLine(d);
+
+        }
+    }
+}
